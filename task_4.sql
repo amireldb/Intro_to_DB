43eda344f211e813387d9a230b4a2c7da3220135
amireldb <eldeebamir6@gmail.com>
2025-06-29 08:44:46 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,16 +1,12 @@
-import mysql.connector
-
-mydb = mysql.connector.connect(
-host = "localhost",
-user = "root" ,
-password = "Aa#01207036888" ,
-database="alx_book_store"
-)
-mycursor = mydb.cursor()
-mycursor.execute(" SELECT COLUMN_NAME,COLUMN_TYPE, IS_NULLABLE,    COLUMN_KEY,   COLUMN_DEFAULT,   EXTRAFROM   INFORMATION_SCHEMA.COLUMNS WHERE AND TABLE_NAME = 'Books'")
-for db in mycursor :
-    print(db)
-mycursor.close()
-mydb.close()
-
-
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books';
