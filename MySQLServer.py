8b4f3cfe7b5e026ff2e4fe4c5f8ee3a88a9efb99
amireldb <eldeebamir6@gmail.com>
2025-06-29 08:17:31 +0300

    Update MySQLServer.py

--- MySQLServer.py
+++ MySQLServer.py
@@ -7,7 +7,7 @@ try:
     password = "Aa#01207036888" 
     )
     mycursor = mydb.cursor()
-    mycursor.execute(" CREATE  DATABASE IF NOT EXISTS alx_book_store")
+    mycursor.execute(" CREATE DATABASE IF NOT EXISTS alx_book_store")
     print("Database 'alx_book_store' created successfully!")
 except mysql.connector.Error as errr:
     print("erroooor :",errr)
