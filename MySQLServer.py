23de2997d09124a4ea8ea00879919f4e00263bc1
amireldb <eldeebamir6@gmail.com>
2025-06-29 08:16:46 +0300

    Create MySQLServer.py

---
+++ MySQLServer.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+try:
+    mydb = mysql.connector.connect(
+    host = "localhost",
+    user = "root" ,
+    password = "Aa#01207036888" 
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute(" CREATE  DATABASE IF NOT EXISTS alx_book_store")
+    print("Database 'alx_book_store' created successfully!")
+except mysql.connector.Error as errr:
+    print("erroooor :",errr)
+finally:
+    mycursor.close
+    mydb.close
+
+
