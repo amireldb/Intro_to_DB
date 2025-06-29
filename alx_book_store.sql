e820e7698eaf2217d81f7d085b09f3de43bd507b
amireldb <eldeebamir6@gmail.com>
2025-06-29 08:04:24 +0300

    Update alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 CREATE TABLE Authors (
-author_id int Primary Key,
+author_id int PRIMARY KEY,
 author_name VARCHAR(215)
 );
-CREATE TABLE Books (book_id int Primary Key,
+CREATE TABLE Books (book_id int PRIMARY KEY,
 title VARCHAR(130),
 author_id int ,
 price DOUBLE,
@@ -13,19 +13,19 @@ Foreign Key (author_id) REFERENCES  Authors(author_id)
 );
 
 CREATE TABLE Customers (
-customer_id int Primary Key,
+customer_id int PRIMARY KEY,
 customer_name VARCHAR(215),
 email VARCHAR(215),
 address TEXT
 );
 CREATE TABLE Orders(
-order_id int Primary Key,
+order_id int PRIMARY KEY,
 customer_id int ,
 order_date DATE ,
 Foreign Key (customer_id) REFERENCES  Customers(customer_id)
 );
 CREATE TABLE Order_Details (
-orderdetailid int Primary Key,
+orderdetailid int PRIMARY KEY,
 order_id int ,
 book_id int ,
 quantity DOUBLE,
